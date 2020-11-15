7c0ce454fe6416c3b3a2e37ce4a7c0ac1cb7b1dc
sayaka.tanaka <syk.851144436121@gmail.com>
2020-11-15 12:14:07 +0000

    fashions

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,7 +1,7 @@
 Cloudinary.config do |config|
-  config.cloud_name = ENV["dmqyprthr"]
-  config.api_key = ENV["666373834146697"]
-  config.api_secret = ENV["fqqBGAGIaU9PCmqIchnXpBQ-1fI"]
+  config.cloud_name = ENV["cloudinary_cloud_name"]
+  config.api_key = ENV["cloudinary_api_key"]
+  config.api_secret = ENV["cloudinary_api_secret"]
   config.secure = true
   config.cdn_subdomain = true
 end
