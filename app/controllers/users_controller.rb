6d8a013e6b3886dd0ec63e00bbd07866e79dcf3e
sayaka.tanaka <syk.851144436121@gmail.com>
2020-11-19 07:40:38 +0000

    fashions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   
   def index
     @users = User.order(id: :desc).page(params[:page]).per(4)
@@ -62,5 +63,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile, :profile_image)
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
   end
 end
